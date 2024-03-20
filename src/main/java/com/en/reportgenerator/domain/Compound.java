7ada401639bbe68c6920a187ea7097d013bba33d
kroshkamedved <kroshkamedved@gmail.com>
2024-03-20 13:58:35 +0200

    refactor:Change Compounf equality field to equivalent

--- src/main/java/com/en/reportgenerator/domain/Compound.java
+++ src/main/java/com/en/reportgenerator/domain/Compound.java
@@ -1,6 +1,6 @@
 package com.en.reportgenerator.domain;
 
-public record Compound(int id, String name, float equality, float mw, double amount,
+public record Compound(int id, String name, float equivalent, float mw, double amount,
                        float moles, String formula, String condition, String synonym, String cas,
                        @SVGColumn String structure) implements AllFieldsToStringReady {
 }
